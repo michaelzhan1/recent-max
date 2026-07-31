331f51353e7b42fbf37381be1ce7d3e0ecbeb64d
Michael Zhan <michaelzhan2001@gmail.com>
2026-07-31 12:24:19 -0400

    graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/michaelzhan1/recent-max/internal/connection"
 	"github.com/michaelzhan1/recent-max/internal/message"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	// open TCP listener on 8081 for the generator to send data in
 	ln, err := connection.NewTCPListener("8081")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer ln.Close()
+
+	// graceful shutdown
+	go func() {
+		<-ctx.Done()
+		log.Println("Shutting down server...")
+		ln.Close() // needed to break out of the AcceptAndHandle loop
+	}()
 
 	for {
 		err := ln.AcceptAndHandle(func(dec *json.Decoder) error {
@@ -26,7 +42,13 @@ func main() {
 			return nil
 		})
 		if err != nil {
+			if errors.Is(err, net.ErrClosed) {
+				break
+			}
+
 			log.Println("Error handling connection:", err)
 		}
 	}
+
+	log.Println("Server stopped.")
 }
