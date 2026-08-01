b3336a0a039c93c5693e1f8288901a0dead7d634
Michael Zhan <michaelzhan2001@gmail.com>
2026-07-31 23:10:28 -0400

    move tcp server to goroutine

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,11 +8,32 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 
 	"github.com/michaelzhan1/recent-max/internal/connection"
 	"github.com/michaelzhan1/recent-max/internal/message"
 )
+
+func runTCPServer(ln *connection.TCPListener) {
+	for {
+		err := ln.AcceptAndHandle(func(dec *json.Decoder) error {
+			var msg message.Message
+			if err := dec.Decode(&msg); err != nil {
+				return err
+			}
+			log.Printf("Received message: %+v\n", msg)
+			return nil
+		})
+		if err != nil {
+			if errors.Is(err, net.ErrClosed) {
+				break
+			}
+
+			log.Println("Error handling connection:", err)
+		}
+	}
+}
 
 func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
@@ -32,23 +53,15 @@ func main() {
 		ln.Close() // needed to break out of the AcceptAndHandle loop
 	}()
 
-	for {
-		err := ln.AcceptAndHandle(func(dec *json.Decoder) error {
-			var msg message.Message
-			if err := dec.Decode(&msg); err != nil {
-				return err
-			}
-			log.Printf("Received message: %+v\n", msg)
-			return nil
-		})
-		if err != nil {
-			if errors.Is(err, net.ErrClosed) {
-				break
-			}
+	// waitgroups for connection handling
+	var wg sync.WaitGroup
+	wg.Go(func() {
+		log.Println("TCP server is running on port 8081.")
+		runTCPServer(ln)
+	})
 
-			log.Println("Error handling connection:", err)
-		}
-	}
+	<-ctx.Done()
 
+	wg.Wait()
 	log.Println("Server stopped.")
 }
